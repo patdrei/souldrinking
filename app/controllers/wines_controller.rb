d2a11bd1d23783d75b3e5d1dda101c8e086ec5c6
Patrick Dreier <patrick3dreier@googlemail.com>
2020-10-05 22:26:14 +0200

    controllers added

---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,14 @@
+class WinesController < ApplicationController
+  def index
+    @wines = Wine.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'property_preferences/create'
+  get 'properties/index'
+  get 'wines/index'
+  get 'wines/show'
+  get 'wines/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
