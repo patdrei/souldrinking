786f7bf577ef5b10a2acb0b17206dba8ac6fc1e8
sfsharleux <s.harleux@gmail.com>
2020-11-08 14:14:03 +0100

    start routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get 'property_preferences/create'
-  get 'properties/index'
-  get 'wines/index'
-  get 'wines/show'
-  get 'wines/edit'
   devise_for :users
   root to: 'pages#home'
+
+  ressources :wines, only: [ :index, :show, :edit, :update ] do
+    ressources :properties, only: [ :index ]
+    ressources :review, only: [ :new, :create ]
+  end
+  ressources :reviews, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+  # get 'property_preferences/create'
+  # get 'properties/index'
+  # get 'wines/index'
+  # get 'wines/show'
+  # get 'wines/edit'
