c1330cf9a4b1d1060867114c5f2615acc284c32b
sfsharleux <s.harleux@gmail.com>
2020-11-08 14:23:40 +0100

    admin gem instaled

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'property_preferences/create'
-  get 'properties/index'
-  get 'wines/index'
-  get 'wines/show'
-  get 'wines/edit'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # get 'property_preferences/create'
+  # get 'properties/index'
+  # get 'wines/index'
+  # get 'wines/show'
+  # get 'wines/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
