42793cf4c354965a3d5a079cbcd79e45c658b7e1
sfsharleux <s.harleux@gmail.com>
2020-11-14 13:14:15 +0100

    fix up

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users
-  root to: 'pages#home'
-
-  resources :wines, only: [ :index, :show, :edit, :update ] do
-    resources :properties, only: [ :index ]
-    resources :review, only: [ :new, :create ]
-  end
-  resources :reviews, only: [ :destroy ]
+  root to: 'page#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
-
-  # get 'property_preferences/create'
-  # get 'properties/index'
-  # get 'wines/index'
-  # get 'wines/show'
-  # get 'wines/edit'
